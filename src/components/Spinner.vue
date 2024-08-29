4cc7978f192ae011349e1525af5e002a101b156f
Mardoqueiro <mordecai.dimbu@gmail.com>
2024-08-29 12:12:34 +0200

    Components

---
+++ src/components/Spinner.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="spinner-grow text-secondary" role="status">
+    <span class="visually-hidden">Loading...</span>
+  </div>
+
+  <!-- <div class="d-flex justify-content-center">
+    <div class="spinner-border" role="status"></div>
+  </div> -->
+</template>
+
+<script>
+export default {
+  name: "SpinnerComp",
+};
+</script>
